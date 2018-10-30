26dd3dab1cb960e30fcd503d6d4d2ff44505bd92
stevenlee343 <steven2602>
2018-10-30 15:36:44 +0800

    ATM 1030 resultOK

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_LOGIN = 100;
     boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (!logon) {
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivityForResult(intent,100);
+            startActivityForResult(intent,RC_LOGIN);
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100){
+        if(requestCode == RC_LOGIN){
             if(resultCode != RESULT_OK){
-                finish(); 
+                finish();
             }
         }
     }
